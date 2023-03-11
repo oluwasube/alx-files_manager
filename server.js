b89adc35d9082c6e3d51ae7749c2c44d03d3b802
oluwasube <enibeke20@gmail.com>
2023-03-11 20:18:54 +0100

    :zap:improve performance

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import express from 'express';
-import router from './routes/index';
-
-const port = parseInt(process.env.PORT, 10) || 5000;
+const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
 
-app.use(express.json());
-app.use('/', router);
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+// eslint-disable-next-line jest/require-hook
+app.use(express.json());
+// eslint-disable-next-line jest/require-hook
+app.use('/', routes);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
+}
 
 export default app;
